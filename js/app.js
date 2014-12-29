3fe4d7694b35ee1af5826b35b75883714f99906e
Kristoffer Dalby <kradalby@kradalby.no>
2014-12-29 21:48:27 +0100

    Things went wrong...

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 'use strict'
 
+var API_URL = "http://localhost:8080/"
+
 var app = app || {};
+
+var Meeting = function(data) {
+    this.title = m.prop(data.Title)
+    this.date = m.prop(data.Date)
+    this.image = m.prop(data.Image)
+    this.information = m.prop(data.Information)
+}
 
 var home = {
     controller: function() {},
@@ -34,10 +43,35 @@ var pictures = {
 }
 
 var meetings = {
-    controller: function() {},
+    list: function() {
+        return m.request({
+            method: "GET", 
+            url: API_URL + "meetings", 
+            unwrapSuccess: function (response) {
+                return response.meetings
+            },
+            type: Meeting,
+        })
+    },
+    vm: {
+        init: function() {
+            this.meetings = new meetings.list()
+        }
+    },
+    controller: function() {
+        meetings.vm.init()
+    },
     view: function() {
+        var meets = meetings.vm.meetings().map(function(meet) {
+            return [
+                m('h1', meet.title()),
+                m('p', dateToString(new Date(meet.date() * 1000))),
+                m('information', meet.information())
+            ]
+        })
         return m('div', [
             m('h1', "Møter"),
+            meets
         ])
     }
 }
@@ -59,6 +93,13 @@ var contact = {
         ])
     }
 }
+
+var dateToString = function(date) {
+    var local = new Date(date);
+    local.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+    return local.toJSON().slice(0, 10);
+}
+
 
 m.route.mode = 'hash'
 m.route(document.getElementById('right'), '/', {
