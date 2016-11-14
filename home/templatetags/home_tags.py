a7a9d7f1b37456e94edf63933d3c34ac0f140e24
Kristoffer Dalby <kradalby@kradalby.no>
2016-11-14 18:37:47 +0100

    initial rework to wagtail

---
+++ home/templatetags/home_tags.py
@@ -0,0 +1,22 @@
+from datetime import date
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+
+# settings value
+@register.assignment_tag
+def get_google_maps_key():
+    return getattr(settings, 'GOOGLE_MAPS_KEY', "")
+
+
+@register.assignment_tag(takes_context=True)
+def get_site_root(context):
+    # NB this returns a core.Page, not the implementation-specific model used
+    # so object-comparison to self will return false as objects would differ
+    return context['request'].site.root_page
+
+
+def has_menu_children(page):
+    return page.get_children().live().in_menu().exists()
